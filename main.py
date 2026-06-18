3aab33f1d32e3312e26f724136161d4fcd1632a0
CTH-Fan-Club <chesdre628@gmail.com>
2026-06-18 22:49:05 +0800

    v5

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.insert(0, os.path.dirname(__file__))
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger

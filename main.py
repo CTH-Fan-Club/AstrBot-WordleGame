c23801c4486a753e7452779b6941af1dd6f52e9e
CTH-Fan-Club <chesdre628@gmail.com>
2026-06-18 22:37:47 +0800

    v4

--- main.py
+++ main.py
@@ -1,54 +1,53 @@
-from nakuru.entities.components import *
-from nakuru import (
-    GroupMessage,
-    FriendMessage
-)
-from botpy.message import Message, DirectMessage
-from model.platform.qq import QQ
-import time
-from cores.qqbot.global_object import AstrMessageEvent
+from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger
+from wordle import WordleGame
 
-'''
-注意改插件名噢！格式：XXXPlugin 或 Main
-小提示：把此模板仓库 fork 之后 clone 到机器人文件夹下的 addons/plugins/ 目录下，然后用 Pycharm/VSC 等工具打开可获更棒的编程体验（自动补全等）
-'''
-class HelloWorldPlugin:
-    """
-    初始化函数, 可以选择直接pass
-    """
-    def __init__(self) -> None:
-        print("hello, world!")
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+class MyPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
 
-    """
-    机器人程序会调用此函数。
-    返回规范: bool: 插件是否响应该消息 (所有的消息均会调用每一个载入的插件, 如果不响应, 则应返回 False)
-             Tuple: Non e或者长度为 3 的元组。如果不响应, 返回 None； 如果响应, 第 1 个参数为指令是否调用成功, 第 2 个参数为返回的消息链列表, 第 3 个参数为指令名称
-    例子：一个名为"yuanshen"的插件；当接收到消息为“原神 可莉”, 如果不想要处理此消息，则返回False, None；如果想要处理，但是执行失败了，返回True, tuple([False, "请求失败。", "yuanshen"]) ；执行成功了，返回True, tuple([True, "结果文本", "yuanshen"])
-    """
-    def run(self, ame: AstrMessageEvent):
-        if ame.message_str == "helloworld":
-            return True, tuple([True, "Hello World!!", "helloworld"])
-        else:
-            return False, None
+    async def initialize(self):
+        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
 
-    """
-    插件元信息。
-    当用户输入 plugin v 插件名称 时，会调用此函数，返回帮助信息。
-    返回参数要求(必填)：dict{
-        "name": str, # 插件名称
-        "desc": str, # 插件简短描述
-        "help": str, # 插件帮助信息
-        "version": str, # 插件版本
-        "author": str, # 插件作者
-        "repo": str, # 插件仓库地址 [ 可选 ]
-        "homepage": str, # 插件主页  [ 可选 ]
-    }
-    """        
-    def info(self):
-        return {
-            "name": "helloworld",
-            "desc": "测试插件",
-            "help": "测试插件, 回复 helloworld 即可触发",
-            "version": "v1.2",
-            "author": "Soulter"
-        }
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("helloworld")
+    async def helloworld(self, event: AstrMessageEvent):
+        """这是一个 hello world 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        user_name = event.get_sender_name()
+        message_str = event.message_str # 用户发的纯文本消息字符串
+        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+        yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
+from astrbot.api.star import Context, Star, register
+from astrbot.api import logger
+
+@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
+class MyPlugin(Star):
+    def __init__(self, context: Context):
+        super().__init__(context)
+
+    async def initialize(self):
+        """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
+
+    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
+    @filter.command("wordle")
+    async def wordle(self, event: AstrMessageEvent):
+        """这是一个 wordle 指令""" # 这是 handler 的描述，将会被解析方便用户了解插件内容。建议填写。
+        user_name = event.get_sender_name()
+        message_str = event.message_str # 用户发的纯文本消息字符串
+        message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
+        logger.info(message_chain)
+        new_wordle=WordleGame();
+        new_wordle.__init__();
+        new_wordle.start_game();
+        yield event.plain_result(f"开始Wordle, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    async def terminate(self):
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
+
+    async def terminate(self):
+        """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""

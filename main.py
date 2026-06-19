58c0d13db9e8fd6193b931f275d8b2a9061b2330
CTH-Fan-Club <chesdre628@gmail.com>
2026-06-19 10:38:02 +0800

    v14

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ ensure_playwright()
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
 from astrbot.api import logger
-from wordle import WordleGameAsync
+from .wordle import WordleGameAsync
 
 @register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
 class MyPlugin(Star):
